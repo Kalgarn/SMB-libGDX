1734a10a064e079e9102742aeb87a5ced7573297
Kalgarn <kalgarn@tuta.io>
2016-01-13 15:59:44 +0100

    #1 commit

---
+++ desktop/src/com/kalgarn/supermariobros/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.kalgarn.supermariobros.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.kalgarn.supermariobros.SuperMarioBros;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SuperMarioBros(), config);
+	}
+}
